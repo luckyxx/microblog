48807f8e492928f0d49a9ca3afe2d37476d39754
e <mxiaoxuan1998@sina.com>
2019-08-29 16:24:46 +0800

    "paging finished"

--- config.py
+++ config.py
@@ -7,3 +7,6 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    ADMINS = ['mxiaoxuan1998@sina.com']
+    POSTS_PER_PAGE = 2
